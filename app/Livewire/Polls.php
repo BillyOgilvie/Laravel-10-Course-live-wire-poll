675e3a4b7d3859a2f55756b9432184b850150966
Billy Ogilvie <billy.ogilvie@drewberry.co.uk>
2024-06-13 08:38:09 +0100

    101. Voting in Polls!

--- app/Livewire/Polls.php
+++ app/Livewire/Polls.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Poll;
+use App\Models\Option;
 use Livewire\Component;
 
 class Polls extends Component
@@ -18,5 +19,10 @@ class Polls extends Component
             ->latest()->get();
 
         return view('livewire.polls', ['polls' => $polls]);
+    }
+
+    public function vote(Option $option)
+    {
+        $option->votes()->create();
     }
 }
